8e76f94abc606c414a938ac5b7b0486383edc2fe
katekiselevich <kiselkath@gmail.com>
2025-07-01 15:41:40 +0200

    init commit

---
+++ src/test/java/com/managment_system/hospital_api/HospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managment_system.hospital_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
